27050f337762c918b9eb447bc30c0387f247e64f
Pranavgrover <123378465+Pranavgrover@users.noreply.github.com>
2023-02-03 17:28:45 +0530

    Update and rename Assignment1.py.txt to Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,24 @@
+a=2
+a1=3
+b=2.1
+b1=3.1
+c=[1, 2, 3, "list"]
+c1=[1, 1.2]
+d=(1, 2, 3, "list")
+d1=(1, ,3, 4.22)
+e={"name":"Pranav", "Age":19}
+e1{"college":"Bits"}
+f="true"
+f1="false"
+g="c"
+g1="d"
+h="char"
+h1="string"
+print(a, a1)
+print(b, b1)
+print(c, c1)
+print(d, d1)
+print(e, e1)
+print(f, f1)
+print(g, g1)
+print(h, h1)
